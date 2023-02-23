0e63962a7446fdfc8e9aeec42e0b511e037ef4bd
camcamnguyen <camcamnguyen583@gmail.com>
2023-02-22 17:04:23 -0800

    edit wait function

--- bin/wait/Wait.cpp
+++ bin/wait/Wait.cpp
@@ -3,7 +3,9 @@
 #include <string.h>
 #include <errno.h>
 #include <unistd.h>
-#include "Wait.h"
+#include <ProcessClient.h> //check system headers
+#include <sys/wait.h>
+#include "Wait.h" //in the same directory
 
 Wait::Wait(int argc, char **argv)
 	: POSIXApplication(argc, argv)
@@ -18,5 +20,20 @@ Wait::~Wait()
 
 Wait::Result Wait::exec()
 {
+	//get the pid with ProcessClient
+	ProcessClient process;
+	ProcessID pid;
+
+	if() {
+		//call waitpid here
+		//waitpid has 3 parameters: pid, int, int
+		waitpid(pid, 0, 0);
+	}
+	else {
+		//if the pid does not exist, return an error message
+		return InvalidArgument;
+	}
+
+	return Success;
 }
 
